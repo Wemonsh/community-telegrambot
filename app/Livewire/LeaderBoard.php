56b999983422c3bf70d3a3b1bfb54b5d3a9fd134
Oleg Semenov <wemonsh.gg@gmail.com>
2024-01-16 09:48:49 +0300

    Поправил работу доски лидеров 2

--- app/Livewire/LeaderBoard.php
+++ app/Livewire/LeaderBoard.php
@@ -10,9 +10,18 @@ class LeaderBoard extends Component
 {
     public function render()
     {
+        if (Carbon::now()->day > 17) {
+            $start = Carbon::now()->startOfMonth()->addDays(17);
+            $end = Carbon::now()->endOfMonth()->addDays(17);
+        } else {
+            $start = Carbon::now()->subMonth()->startOfMonth()->addDays(17);
+            $end = Carbon::now()->startOfMonth()->addDays(17);
+        }
+
+
         $visits = OfficeVisit::with('telegram_user')
             ->selectRaw('count(*) as total, telegram_user_id')
-            ->whereBetween('created_at', [Carbon::now()->startOfMonth()->format('Y-m-d H:i:s'), Carbon::now()->endOfMonth()->format('Y-m-d H:i:s')])
+            ->whereBetween('created_at', [$start->format('Y-m-d H:i:s'), $end->format('Y-m-d H:i:s')])
             ->groupBy('telegram_user_id')
             ->orderByDesc('total')
             ->get();
